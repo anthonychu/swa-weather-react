c35a7235de518f3d6e30d430c7899bbdf3b0b0f3
Anthony Chu <anthony@anthonychu.ca>
2021-07-22 14:31:59 -0700

    Add functions

--- src/apiWeatherProvider/useApiWeather.js
+++ src/apiWeatherProvider/useApiWeather.js
@@ -64,196 +64,24 @@ export const fetchReducer = (state, { type, payload }) => {
 };
 
 const useApiWeather = (options) => {
-  const endpoint = 'https://atlas.microsoft.com/weather/currentConditions/json';
   const [state, dispatch] = useReducer(fetchReducer, initialState);
   const { data, errorMessage } = state;
   const [isLoading, setIsLoading] = useState(false);
   const { lang, key, lon, lat } = options;
-  const params = {
-    'subscription-key': key,
-    'api-version': '1.0'
-  };
 
   const fetchData = async () => {
+    const params = {};
     if (!lat || !lon) {
       const location = await getLocation();
-      params.query = `${location.latitude},${location.longitude}`;
+      params.coordinates = `${location.latitude},${location.longitude}`;
     } else {
-      params.query = `${lat},${lon}`;
+      params.coordinates = `${lat},${lon}`;
     }
   
     setIsLoading(true);
     try {
-      const forecastResponse = await axios.get(endpoint, { params });
+      const forecastResponse = await axios.get('/api/weather', { params });
       const forecastResponseData = forecastResponse.data;
-      // const forecastResponseData = {
-      //   "results": [
-      //     {
-      //       "dateTime": "2021-07-22T11:08:00-07:00",
-      //       "phrase": "Mostly sunny",
-      //       "iconCode": 2,
-      //       "hasPrecipitation": false,
-      //       "isDayTime": true,
-      //       "temperature": {
-      //         "value": 21.1,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "realFeelTemperature": {
-      //         "value": 26.6,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "realFeelTemperatureShade": {
-      //         "value": 20.3,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "relativeHumidity": 60,
-      //       "dewPoint": {
-      //         "value": 13.1,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "wind": {
-      //         "direction": {
-      //           "degrees": 338.0,
-      //           "localizedDescription": "NNW"
-      //         },
-      //         "speed": {
-      //           "value": 3.5,
-      //           "unit": "km/h",
-      //           "unitType": 7
-      //         }
-      //       },
-      //       "windGust": {
-      //         "speed": {
-      //           "value": 7.7,
-      //           "unit": "km/h",
-      //           "unitType": 7
-      //         }
-      //       },
-      //       "uvIndex": 7,
-      //       "uvIndexPhrase": "High",
-      //       "visibility": {
-      //         "value": 4.8,
-      //         "unit": "km",
-      //         "unitType": 6
-      //       },
-      //       "obstructionsToVisibility": "",
-      //       "cloudCover": 15,
-      //       "ceiling": {
-      //         "value": 10058.0,
-      //         "unit": "m",
-      //         "unitType": 5
-      //       },
-      //       "pressure": {
-      //         "value": 1022.0,
-      //         "unit": "mb",
-      //         "unitType": 14
-      //       },
-      //       "pressureTendency": {
-      //         "localizedDescription": "Steady",
-      //         "code": "S"
-      //       },
-      //       "past24HourTemperatureDeparture": {
-      //         "value": 4.2,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "apparentTemperature": {
-      //         "value": 21.1,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "windChillTemperature": {
-      //         "value": 21.1,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "wetBulbTemperature": {
-      //         "value": 16.1,
-      //         "unit": "C",
-      //         "unitType": 17
-      //       },
-      //       "precipitationSummary": {
-      //         "pastHour": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past3Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past6Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past9Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past12Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past18Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         },
-      //         "past24Hours": {
-      //           "value": 0.0,
-      //           "unit": "mm",
-      //           "unitType": 3
-      //         }
-      //       },
-      //       "temperatureSummary": {
-      //         "past6Hours": {
-      //           "minimum": {
-      //             "value": 12.5,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           },
-      //           "maximum": {
-      //             "value": 21.1,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           }
-      //         },
-      //         "past12Hours": {
-      //           "minimum": {
-      //             "value": 12.5,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           },
-      //           "maximum": {
-      //             "value": 21.1,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           }
-      //         },
-      //         "past24Hours": {
-      //           "minimum": {
-      //             "value": 12.5,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           },
-      //           "maximum": {
-      //             "value": 21.3,
-      //             "unit": "C",
-      //             "unitType": 17
-      //           }
-      //         }
-      //       }
-      //     }
-      //   ]
-      // };
 
       const payload = mapData(
         forecastResponseData.results[0],
