0c78c13542cd15b1d46ff4118a0b6af25b249f22
Anthony Chu <anthony@anthonychu.ca>
2021-07-23 07:53:09 -0700

    Clean up code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import './App.css';
-import { getLocation } from './utils';
-import { useEffect } from 'react';
 import ReactWeather from 'react-open-weather';
 import { useApiWeather } from './apiWeatherProvider';
 
 function App() {
-  useEffect(() => {
-    (async function () {
-      try {
-        const location = await getLocation();
-        console.log(location);
-        
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  });
 
   const { data, isLoading, errorMessage } = useApiWeather({
     key: '',
